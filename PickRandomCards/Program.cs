8b80497367af549ceb0e562703b63743c8dec17a
MAULI <MAULI@DESKTOP-Q3FDPPQ>
2022-04-08 09:03:57 +0530

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class PickRandomCards
+{
+    static void Main(string[] args)
+    { 
+        Console.Write("Enter the number of cards to pick: ");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+        string line = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+        if (int.TryParse(line, out int numberOfCards))
+        {
+            foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+            {
+                Console.WriteLine(card);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid number.");
+        }
+     }
+}
+ 
